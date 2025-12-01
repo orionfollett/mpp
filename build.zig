fb99c6f9252c2c4797279efa0e93066a7229086c
Orion Follett <orion.follett@firstresonance.io>
2025-11-30 21:44:21 -0800

    raylib built and running

--- build.zig
+++ build.zig
@@ -3,6 +3,11 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
+    const raylib_dep = b.dependency("raylib_zig", .{ .target = target, .optimize = optimize, .linkage = .static });
+
+    const raylib = raylib_dep.module("raylib"); // main raylib module
+    const raygui = raylib_dep.module("raygui"); // raygui module
+    const raylib_artifact = raylib_dep.artifact("raylib"); // raylib C library
     const exe = b.addExecutable(.{
         .name = "mpp",
         .root_module = b.createModule(.{
@@ -11,6 +16,9 @@ pub fn build(b: *std.Build) void {
             .optimize = optimize,
         }),
     });
+    exe.linkLibrary(raylib_artifact);
+    exe.root_module.addImport("raylib", raylib);
+    exe.root_module.addImport("raygui", raygui);
 
     b.installArtifact(exe);
 
